ab9a9d3307979d8bb367ec79844c319b6763882a
melekch14 <malek.chaari96@gmail.com>
2025-05-26 00:53:34 +0100

    add client order interface

--- front/src/app/components/client-create-order/client-create-order.component.ts
+++ front/src/app/components/client-create-order/client-create-order.component.ts
@@ -6,4 +6,64 @@ import { Component } from '@angular/core';
   styleUrls: ['./client-create-order.component.css'],
   standalone: false
 })
-export class ClientCreateOrderComponent { } 
+export class ClientCreateOrderComponent {
+  order = {
+    od: { sphere: '', cylinder: '', axe: '', addition: '' },
+    og: { sphere: '', cylinder: '', axe: '', addition: '' },
+    lastName: '',
+    firstName: '',
+    phone: '',
+    email: '',
+    supplement: '',
+    traitement: ''
+  };
+
+  traitements = [
+    'Antireflet',
+    'Durci',
+    'Photochromique',
+    'Polarisant',
+    'Blue Cut'
+  ];
+
+  selectedFileName: string = '';
+
+  copyToOG() {
+    this.order.og.sphere = this.order.od.sphere;
+    this.order.og.cylinder = this.order.od.cylinder;
+    this.order.og.axe = this.order.od.axe;
+    this.order.og.addition = this.order.od.addition;
+  }
+
+  copyToOD() {
+    this.order.od.sphere = this.order.og.sphere;
+    this.order.od.cylinder = this.order.og.cylinder;
+    this.order.od.axe = this.order.og.axe;
+    this.order.od.addition = this.order.og.addition;
+  }
+
+  copyFieldToOG(field: 'sphere' | 'cylinder' | 'axe' | 'addition') {
+    this.order.og[field] = this.order.od[field];
+  }
+
+  onSupplementChange() {
+    // Reset file name if supplement changes
+    if (this.order.supplement !== 'precal') {
+      this.selectedFileName = '';
+    }
+  }
+
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      this.selectedFileName = file.name;
+    } else {
+      this.selectedFileName = '';
+    }
+  }
+
+  submitOrder() {
+    // TODO: Implement order submission logic
+    alert('Commande soumise!');
+  }
+} 

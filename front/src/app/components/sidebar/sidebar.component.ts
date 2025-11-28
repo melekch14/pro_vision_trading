70680649801b8a652f318fe465ad898ae2f96d36
melekch14 <chaarimelek96@gmail.com>
2025-11-28 02:37:42 +0100

    update

--- front/src/app/components/sidebar/sidebar.component.ts
+++ front/src/app/components/sidebar/sidebar.component.ts
@@ -80,7 +80,7 @@ export class SidebarComponent implements OnInit {
     },
     {
       id: 'opticiens',
-      name: 'Equipes',
+      name: 'Equipe',
       icon: 'person',
       route: '/app/opticiens'
     },

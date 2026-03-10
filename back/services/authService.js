156d8ace310062948d36b2e1a077cdb7b56bb9fa
melekch14 <chaarimelek96@gmail.com>
2026-03-11 00:48:43 +0100

    update

--- back/services/authService.js
+++ back/services/authService.js
@@ -126,7 +126,7 @@ const sendPasswordByEmail = async (email) => {
             pass: "bxoiuulfoyfsrcrl"
         }
     });
-    const resetUrl = `http://213.32.16.132:4200/reset-password?token=${token}`;
+    const resetUrl = `http://135.125.202.253:4200/reset-password?token=${token}`;
 
     const mailOptions = {
         from: '"Pro Vision Trading" <provisiontrading38@gmail.com>',
@@ -201,7 +201,7 @@ const requestPasswordReset = async (email) => {
             pass: "bxoiuulfoyfsrcrl"
         }
     });
-    const resetUrl = `http://213.32.16.132:4200/reset-password?token=${token}`;
+    const resetUrl = `http://135.125.202.253:4200/reset-password?token=${token}`;
 
     const mailOptions = {
         from: '"Pro Vision Trading" <provisiontrading38@gmail.com>',
